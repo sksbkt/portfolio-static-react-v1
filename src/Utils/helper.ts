adb0583a0a224ba038fbb8fc9db6b945a564c993
atenziono <atenziono@gmail.com>
2024-04-30 01:13:02 +0430

    figma clone added to the list

--- src/Utils/helper.ts
+++ src/Utils/helper.ts
@@ -1,55 +1,57 @@
-import projImg1 from '../assets/img/project-img1.png';
-import quantic_shop_home from '../assets/img/projects-img/quantic_shop_home.png';
-import quantic_machines from '../assets/img/projects-img/quantic_machines_home.png';
-import quantic_swrtodo from '../assets/img/projects-img/quantic_swrtodo.png';
-import quantic_todo from '../assets/img/projects-img/quantic_todo.png';
-import promptopia_quantic from '../assets/img/projects-img/promptopia_quantic.png';
-import quantic_dashboard from '../assets/img/projects-img/quantic_dashboard.png';
-import projImg2 from '../assets/img/project-img2.png';
-import projImg3 from '../assets/img/project-img3.png';
+import projImg1 from "../assets/img/project-img1.png";
+import quantic_shop_home from "../assets/img/projects-img/quantic_shop_home.png";
+import quantic_machines from "../assets/img/projects-img/quantic_machines_home.png";
+import quantic_swrtodo from "../assets/img/projects-img/quantic_swrtodo.png";
+import quantic_figpro from "../assets/img/projects-img/figma_clone.png";
+import promptopia_quantic from "../assets/img/projects-img/promptopia_quantic.png";
+import quantic_dashboard from "../assets/img/projects-img/quantic_dashboard.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
 export const projects = [
-    {
-        title: "Quantic shop",
-        description: "an e-commerce website",
-        imgUrl: quantic_shop_home,
-        link: 'https://quantic-shop.netlify.app/'
-    },
-    {
-        title: "Quantic dashboard",
-        description: "A dashboard and profile management system",
-        imgUrl: quantic_dashboard,
-        link: 'https://quantic-dashboard.vercel.app/'
-    },
-    {
-        title: "Promptopia",
-        description: "A blog for sharing prompt for AI services",
-        imgUrl: promptopia_quantic,
-        link: 'https://promptopia-quantic.vercel.app/'
-    },
-    {
-        title: "Quantic machines",
-        description: "Portfolio page",
-        imgUrl: quantic_machines,
-        link: '#home'
-    },
-    {
-        title: "Quantic SWR todo",
-        description: "A todo list showcasing SWR",
-        imgUrl: quantic_swrtodo,
-        link: 'https://quantic-swrtodo.netlify.app/'
-    },
-    {
-        title: "Quantic todo",
-        description: "Another todo app showcasing drag and drop and element handling in 2D space",
-        imgUrl: quantic_todo,
-        link: 'https://quantic-todo.netlify.app/'
-    },
+  {
+    title: "Quantic shop",
+    description: "an e-commerce website",
+    imgUrl: quantic_shop_home,
+    link: "https://quantic-shop.netlify.app/",
+  },
+  {
+    title: "Figma clone",
+    description: "a project to showcase fabric js and liveblock usage",
+    imgUrl: quantic_figpro,
+    link: "https://figma-clone-quantic.vercel.app/",
+  },
+  {
+    title: "Quantic dashboard",
+    description: "A dashboard and profile management system",
+    imgUrl: quantic_dashboard,
+    link: "https://quantic-dashboard.vercel.app/",
+  },
+  {
+    title: "Promptopia",
+    description: "A blog for sharing prompt for AI services",
+    imgUrl: promptopia_quantic,
+    link: "https://promptopia-quantic.vercel.app/",
+  },
+  {
+    title: "Quantic machines",
+    description: "Portfolio page",
+    imgUrl: quantic_machines,
+    link: "#home",
+  },
+  {
+    title: "Quantic SWR todo",
+    description: "A todo list showcasing SWR",
+    imgUrl: quantic_swrtodo,
+    link: "https://quantic-swrtodo.netlify.app/",
+  },
 ];
 
 export const strings = {
-    aboutMe_desc: `I am a front-end developer with 5+ years of experience building user interfaces for websites and web applications using React and Next.js. I have a deep understanding of both frameworks and I am proficient in using them to create fast, scalable, and SEO-friendly applications.
+  aboutMe_desc: `I am a front-end developer with 5+ years of experience building user interfaces for websites and web applications using React and Next.js. I have a deep understanding of both frameworks and I am proficient in using them to create fast, scalable, and SEO-friendly applications.
     I am passionate about creating user-friendly and visually appealing interfaces. I have a keen eye for detail and I am always looking for new ways to improve the user experience. I am also a strong advocate for accessibility, and I ensure that all of my interfaces are accessible to users with disabilities.
     In addition to my technical skills, I am also a team player and I am always willing to go the extra mile to help my team succeed. I am also a quick learner and I am always eager to take on new challenges.`,
-    projects_desc: 'I am proud of the work that I have done on these projects, and I believe that they demonstrate my skills and experience in frontend web development using React and Next.js. I am always looking for new and challenging projects to work on, and I am excited to continue learning and growing as a developer.',
-    skills_desc: 'I am confident that I have the skills and experience necessary to be a successful frontend web developer. I am eager to learn new things and contribute to a team.'
-}
+  projects_desc:
+    "I am proud of the work that I have done on these projects, and I believe that they demonstrate my skills and experience in frontend web development using React and Next.js. I am always looking for new and challenging projects to work on, and I am excited to continue learning and growing as a developer.",
+  skills_desc:
+    "I am confident that I have the skills and experience necessary to be a successful frontend web developer. I am eager to learn new things and contribute to a team.",
+};
