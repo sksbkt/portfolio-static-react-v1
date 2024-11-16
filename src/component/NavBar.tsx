8e238c7a1321c813b37d1349cc196c74d8a27b20
atenziono <atenziono@gmail.com>
2024-11-16 12:59:03 +0330

    updated social buttons, navigation, and replace circle progress images with actual components

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -1,68 +1,128 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import logo from "/src/assets/img/logo_dark.png";
 import linkedin from "../assets/img/nav-icon1.svg";
-import facebook from "../assets/img/nav-icon2.svg";
+import github from "../assets/img/nav-icon4.svg";
 import instagram from "../assets/img/nav-icon3.svg";
 export default function NavBar() {
-    const [activeLink, setActiveLink] = useState('home');
-    const [scrolled, setScrolled] = useState(false);
-    useEffect(() => {
-        function onScroll() {
-            if (window.scrollY > 50) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        }
-        window.addEventListener('scroll', onScroll);
-        //? like this we are removing event lister when the component gets unmounted
-        return () => window.removeEventListener('scroll', onScroll);
-    }, []);
-
-
-    function linkClass(link: string) {
-        if (activeLink === link) {
-            return 'active navbar-link';
-        } else {
-            return 'navbar-link';
-        }
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    function onScroll() {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
     }
+    window.addEventListener("scroll", onScroll);
+    //? like this we are removing event lister when the component gets unmounted
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
-    function onUpdateActiveLink(value: string) {
-        setActiveLink(value);
+  function linkClass(link: string) {
+    if (activeLink === link) {
+      return "active navbar-link";
+    } else {
+      return "navbar-link";
     }
+  }
 
-    return (
-        <Navbar expand="lg" className={scrolled ? 'scrolled' : ''}>
-            <Container>
-                <Navbar.Brand href="#home">
-                    <img src={logo} className="logo" alt="Logo" />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav">
-                    <span className="navbar-toggler-icon"></span>
-                </Navbar.Toggle>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home" className={linkClass('home')}>Home</Nav.Link>
-                        <Nav.Link href="#skills" className={linkClass('skills')} >Skills</Nav.Link>
-                        <Nav.Link href="#projects" className={linkClass('projects')}>Projects</Nav.Link>
-                    </Nav>
-                    <span className="navbar-text">
-                        <div className="social-icon">
-                            <a href=""><img src={linkedin} alt="" /></a>
-                            <a href=""><img src={facebook} alt="" /></a>
-                            <a href=""><img src={instagram} alt="" /></a>
-                        </div>
-                        <Button
-                            className="vvd"
-                            onClick={() => { console.log('connect'); }}
-                        >
-                            <span>Lets connect</span>
-                        </Button>
-                    </span>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
+  function onUpdateActiveLink(value: string) {
+    setActiveLink(value);
+  }
+
+  function moveToAnchor(ref: string) {
+    const element = document.getElementById(ref);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+
+  return (
+    <Navbar
+      expand="lg"
+      className={scrolled ? "scrolled" : ""}
+    >
+      <Container>
+        <Navbar.Brand
+          href="#home"
+          onClick={() => onUpdateActiveLink("home")}
+        >
+          <img
+            src={logo}
+            className="logo"
+            alt="Logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link
+              href="#home"
+              className={linkClass("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={linkClass("skills")}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={linkClass("projects")}
+            >
+              Projects
+            </Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <SocialIcon
+                href="https://www.linkedin.com/in/ali-khoshbakht/"
+                title="Linkedin"
+                icon={linkedin}
+              />
+              <SocialIcon
+                href="https://github.com/sksbkt"
+                title="Github"
+                icon={github}
+              />
+            </div>
+            <Button
+              className="vvd"
+              onClick={() => {
+                moveToAnchor("contact");
+              }}
+            >
+              <span>Lets connect</span>
+            </Button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+interface SocialIconProps {
+  href: string;
+  title: string;
+  icon: string;
+}
+function SocialIcon({ href, title, icon }: SocialIconProps) {
+  return (
+    <a
+      href={href}
+      target={"_blank"}
+      title={title}
+    >
+      <img
+        src={icon}
+        alt=""
+      />
+    </a>
+  );
 }
--- src/component/Skills.tsx
+++ src/component/Skills.tsx
@@ -1,73 +1,128 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
 import colorSharp from "../assets/img/color-sharp.png";
 import { strings } from "../Utils/helper";
-
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 export default function Skills() {
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
-      items: 5
+      items: 5,
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3
+      items: 3,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2
+      items: 2,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
+      items: 1,
+    },
   };
-  return <section className="skill" >
-    <Container>
-      <Row>
-        <Col>
-          <div className="skill-bx px-5 position-relative">
-            {/* //? we are using this technique to move scroll to a more reasonable position */}
+  return (
+    <section className="skill">
+      <Container>
+        <Row>
+          <Col>
+            <div className="skill-bx px-5 position-relative">
+              {/* //? we are using this technique to move scroll to a more reasonable position */}
 
-            <div className="position-absolute" style={{ transform: 'translateY(-20vh)' }} id="skills"></div>
-            <h2>Skills</h2>
-            <p>{strings.skills_desc}</p>
-            <Carousel
-              className="skill-slider"
-              autoPlay={true}
-              autoPlaySpeed={5000}
-              responsive={responsive}
-              pauseOnHover={true}
-              infinite={true}
-              afterChange={(s, z) => { }}
-            >
-              <div className="item">
-                <img src={meter1} alt="image" />
-                <h5>Web development</h5>
-              </div>
-              <div className="item">
-                <img src={meter2} alt="image" />
-                <h5>Web design</h5>
-              </div>
-              <div className="item">
-                <img src={meter3} alt="image" />
-                <h5>UX/UI</h5>
-              </div>
-              {/* <div className="item">
+              <div
+                className="position-absolute"
+                style={{ transform: "translateY(-20vh)" }}
+                id="skills"
+              ></div>
+              <h2>Skills</h2>
+              <p>{strings.skills_desc}</p>
+              <Carousel
+                className="skill-slider"
+                autoPlay={false}
+                autoPlaySpeed={5000}
+                responsive={responsive}
+                pauseOnHover={true}
+                infinite={true}
+                afterChange={(s, z) => {}}
+              >
+                <ProgressCircle
+                  percentage={95}
+                  title="Web development"
+                />
+                <ProgressCircle
+                  percentage={95}
+                  title="Web design"
+                />
+                <ProgressCircle
+                  percentage={95}
+                  title="UX/UI"
+                />
+
+                {/* <div className="item">
                 <img src={meter1} alt="image" />
                 <h5>Mobile app development</h5>
               </div> */}
-            </Carousel>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-    <img className="background-image-left" src={colorSharp} alt={colorSharp} />
-  </section >;
+              </Carousel>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <img
+        className="background-image-left"
+        src={colorSharp}
+        alt={colorSharp}
+      />
+    </section>
+  );
+}
+
+interface progressCircleProps {
+  percentage: number;
+  title: string;
+}
+
+function ProgressCircle({ percentage, title }: progressCircleProps) {
+  return (
+    <div
+      className="item"
+      // date-tooltip={title}
+      title={title}
+    >
+      <CircularProgressbar
+        styles={{
+          root: {
+            width: "50%",
+            margin: "0 auto 15px auto",
+          },
+          path: {
+            stroke: "#833397",
+            transform: "rotate(45deg)",
+            transformOrigin: "center center",
+          },
+          trail: {
+            stroke: "transparent",
+          },
+          text: {
+            fill: "white",
+            fontWeight: "bold",
+          },
+        }}
+        strokeWidth={11}
+        background={false}
+        counterClockwise={true}
+        circleRatio={0.95}
+        value={percentage}
+        text={`${percentage}%`}
+      />
+      <h5>{title}</h5>
+    </div>
+  );
 }
