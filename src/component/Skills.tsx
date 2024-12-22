8e5b4fb564ed196e87048d2b1ff285ac83e253b5
atenziono <atenziono@gmail.com>
2024-12-22 17:21:40 +0330

    react responsive carousel has been added

--- src/component/Skills.tsx
+++ src/component/Skills.tsx
@@ -4,28 +4,55 @@ import colorSharp from "../assets/img/color-sharp.png";
 import { strings } from "../Utils/helper";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import MultiCarousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import {  useState } from "react";
+const responsive = {
+  desktop: {
+    breakpoint: {
+      max: 3000,
+      min: 1024,
+    },
+    items: 3,
+    partialVisibilityGutter: 40,
+  },
+  tablet: {
+    breakpoint: {
+      max: 1024,
+      min: 464,
+    },
+    items: 1,
+    partialVisibilityGutter: 30,
+  },
+  mobile: {
+    breakpoint: {
+      max: 464,
+      min: 0,
+    },
+    items: 1,
+    partialVisibilityGutter: 30,
+  },
+};
+const skillsSet = {
+  React: {
+    title: "React",
+    proficiency: 95,
+  },
+  NextJs: {
+    title: "NextJs",
+    proficiency: 95,
+  },
+  Javascript: {
+    title: "Javascript",
+    proficiency: 90,
+  },
+  Typescript: {
+    title: "Typescript",
+    proficiency: 90,
+  },
+};
 export default function Skills() {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
+  const [activeIndex, setActiveIndex] = useState(1);
   return (
     <section className="skill">
       <Container>
@@ -41,26 +68,54 @@ export default function Skills() {
               ></div>
               <h2>Skills</h2>
               <p>{strings.skills_desc}</p>
-              <Carousel
-                className="skill-slider"
-                showArrows={true}
-                infiniteLoop={true}
-                showStatus={false}
-                showIndicators={false}
+
+              <MultiCarousel
+                // autoPlay={true}
+                additionalTransfrom={0}
+                arrows
+                autoPlaySpeed={3000}
+                centerMode={false}
+                beforeChange={(prev, { currentSlide, deviceType }) => {
+                  if (deviceType === "desktop") {
+                    setActiveIndex(
+                      (prev + 1) % Object.values(skillsSet).length
+                    );
+                  }
+                }}
+                className=""
+                // containerClass="container-with-dots"
+                dotListClass=""
+                draggable
+                focusOnSelect={false}
+                infinite
+                itemClass=""
+                keyBoardControl
+                minimumTouchDrag={80}
+                pauseOnHover
+                renderArrowsWhenDisabled={false}
+                renderButtonGroupOutside={false}
+                renderDotsOutside={false}
+                responsive={responsive}
+                rewind={false}
+                rewindWithAnimation={false}
+                rtl={false}
+                shouldResetAutoplay
+                showDots={false}
+                sliderClass=""
+                slidesToSlide={1}
+                swipeable
               >
-                <ProgressCircle
-                  percentage={95}
-                  title="Web development"
-                />
-                <ProgressCircle
-                  percentage={95}
-                  title="Web design"
-                />
-                <ProgressCircle
-                  percentage={95}
-                  title="UX/UI"
-                />
-              </Carousel>
+                {Object.values(skillsSet).map((skill, index) => {
+                  return (
+                    <ProgressCircle
+                      percentage={skill.proficiency}
+                      title={skill.title}
+                      key={index}
+                      active={activeIndex === index}
+                    />
+                  );
+                })}
+              </MultiCarousel>
             </div>
           </Col>
         </Row>
@@ -77,42 +132,48 @@ export default function Skills() {
 interface progressCircleProps {
   percentage: number;
   title: string;
+  active: boolean;
 }
 
-function ProgressCircle({ percentage, title }: progressCircleProps) {
+function ProgressCircle({ percentage, title, active }: progressCircleProps) {
   return (
-    <div
-      className="item"
-      // date-tooltip={title}
-      title={title}
-    >
-      <CircularProgressbar
-        styles={{
-          root: {
-            width: "50%",
-            margin: "0 auto 15px auto",
-          },
-          path: {
-            stroke: "#833397",
-            transform: "rotate(45deg)",
-            transformOrigin: "center center",
-          },
-          trail: {
-            stroke: "transparent",
-          },
-          text: {
-            fill: "white",
-            fontWeight: "bold",
-          },
+    <div className="item-container">
+      <div
+        className="item"
+        style={{
+          scale: `${active ? 1 : 0.7}`,
+
+          transition: "all 0.3s",
         }}
-        strokeWidth={11}
-        background={false}
-        counterClockwise={true}
-        circleRatio={0.95}
-        value={percentage}
-        text={`${percentage}%`}
-      />
-      <h5>{title}</h5>
+      >
+        <CircularProgressbar
+          styles={{
+            root: {
+              width: "30%",
+              margin: "0 auto 15px auto",
+            },
+            path: {
+              stroke: "#833397",
+              transform: "rotate(45deg)",
+              transformOrigin: "center center",
+            },
+            trail: {
+              stroke: "transparent",
+            },
+            text: {
+              fill: "white",
+              fontWeight: "bold",
+            },
+          }}
+          strokeWidth={11}
+          background={false}
+          counterClockwise={true}
+          circleRatio={percentage / 100}
+          value={percentage}
+          text={`${percentage}%`}
+        />
+        <h5>{title}</h5>
+      </div>
     </div>
   );
 }
